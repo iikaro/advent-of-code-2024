2070be74582ab699bb01ed7f01669e90f5426eb2
Icaro <icaro@kelda.no>
2024-12-05 10:56:42 +0100

    feat: add utility method that convert elements of a list of strings to integers

--- solution/utils.py
+++ solution/utils.py
@@ -1,5 +1,9 @@
-from typing import Any
+from typing import Any, Iterable
 
 
 def print_solution(solution: Any) -> None:
     print(f"The solution is: {solution}.")
+
+
+def convert_to_int(elements: Iterable[str]) -> list[int]:
+    return [int(element) for element in elements]
