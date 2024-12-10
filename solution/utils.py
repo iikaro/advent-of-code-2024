5b00ec1259998083ece0f0281785f6582fbcbecb
Icaro <icaro@kelda.no>
2024-12-10 21:54:54 +0100

    chore: extend utils functionalities

--- solution/utils.py
+++ solution/utils.py
@@ -1,9 +1,11 @@
 from typing import Any, Iterable
 
+NUMBERS: list[str] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 
 def print_solution(solution: Any) -> None:
     print(f"The solution is: {solution}.")
 
 
 def convert_to_int(elements: Iterable[str]) -> list[int]:
-    return [int(element) for element in elements]
+    return [int(element) for element in elements if element in NUMBERS]
